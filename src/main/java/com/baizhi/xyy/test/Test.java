b399a911bea34c2e77070a07aa40fbc1e84bb435
xyy <903172877@qq.com>
2018-08-08 21:03:32 +0800

    第二次提交

---
+++ src/main/java/com/baizhi/xyy/test/Test.java
@@ -0,0 +1,10 @@
+package com.baizhi.xyy.test;
+
+/**
+ * Created by Administrator on 2018/8/8.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是我第一次测试");
+    }
+}
